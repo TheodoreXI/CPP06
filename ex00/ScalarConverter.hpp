0e345fcada8c569e28d633ff39a319e78d87c20c
Abdelhadi Akroud <aakroud@e3r8p1.1337.ma>
2025-12-29 14:53:51 +0100

    maybe

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,17 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+
+class ScalarConverter
+{
+	public:
+		static void	convert(std::string type);
+		
+};
+
+
+
+
+#endif
